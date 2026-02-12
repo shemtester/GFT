2486b47514e2f783d04db295642b158ce48408a8
shemtester <shemarcanglin@gmail.com>
2026-02-11 21:58:05 -0500

    fixed bugs

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3J2VzHl6VvR3fCdDcK7EsZ1GIttrVCys",
-  authDomain: "gft-1-0.firebaseapp.com",
-  projectId: "gft-1-0",
-  storageBucket: "gft-1-0.firebasestorage.app",
-  messagingSenderId: "454254663763",
-  appId: "1:454254663763:web:cec669ec1267e465680ea6",
-  measurementId: "G-M9HN2JEEQP"
+  apiKey: "AIzaSyD6_qmO6E5TJhi_3HSJ1sKH2JVbDR9o3WM",
+  authDomain: "gift-factory-pos.firebaseapp.com",
+  projectId: "gift-factory-pos",
+  storageBucket: "gift-factory-pos.firebasestorage.app",
+  messagingSenderId: "865882648545",
+  appId: "1:865882648545:web:132f70a7c5b10d35adaca0",
+  measurementId: "G-PB043HHHER"
 };
 
 const app = initializeApp(firebaseConfig);
